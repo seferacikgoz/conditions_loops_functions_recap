25e87deccfd1c4db86051873382a2fe3096dbbed
seferacikgoz <102485608+seferacikgoz@users.noreply.github.com>
2023-01-18 15:18:40 +0300

    arrow func. practice

--- arrowFunctions.js
+++ arrowFunctions.js
@@ -1,2 +1,29 @@
 console.log("**** ARROW FUNCTIONS ****");
 
+// const topla = (a, b) => a + b;
+
+// console.log(topla(2, 3)); //! Arrow function larda otomatik returning özelliği vardır.
+
+// const usAl = (t, u) => t ** u;
+// console.log(usAl(2, 3));
+
+// //? Örnek : Silindir hacmi
+
+// const r = +prompt("Yarıcap Giriniz");
+// const h = +prompt("Yükseklik Giriniz");
+
+// const hacimHesapla = (r, h) => Math.PI * r * r * h;
+
+// console.log(`Hacmi: ${hacimHesapla(r, h).toFixed(2)}`);
+
+
+//? Örnek : Yaş Hesaplama
+
+const tarih = +prompt("Doğum tarihini giriniz")
+
+const yasHesapla = (tarih) => {
+   const yas = new Date().getFullYear() - tarih
+   return yas
+}
+
+console.log("Yaşınız:" + yasHesapla(tarih))
