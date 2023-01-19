6a067084e151f347c241c552efca6eeae64ea7fd
seferacikgoz <102485608+seferacikgoz@users.noreply.github.com>
2023-01-19 13:39:53 +0300

    recursive and scopes

---
+++ scope.js
@@ -0,0 +1,34 @@
+//? ===============================================
+//?             FONKSİYONLAR-SCOPE
+//? ===============================================
+
+
+console.log("**** SCOPE ****");
+
+
+let sayi1 = 5
+console.log(sayi1)
+
+const fonks1 = function () {
+    sayi1 = 22
+    console.log(sayi1);
+}
+
+fonks1()
+
+console.log(sayi1++)
+console.log(sayi1);
+
+//*-----------------------------------
+
+let sayi2 = 7
+console.log(sayi2)
+
+
+const fonks2 = () => {
+    let sayi2 = 11
+    console.log(sayi2);
+}
+
+fonks2()
+console.log(sayi2++);
